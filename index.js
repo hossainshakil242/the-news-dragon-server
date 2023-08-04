68cbbcac6185e5ca7ff2b26f5971451ac6f86946
hossainshakil242 <realshakil8@gmail.com>
2023-08-04 10:46:10 +0600

    ceate single categorie

--- index.js
+++ index.js
@@ -14,6 +14,12 @@ app.get('/categories', (req,res)=>{
     res.send (categories)
 })
 
+app.get(`/categories/:id`, (req, res)=>{
+    const id = parseInt(req.params.id);
+    const categorie =  categories.find(item=> item.id == id);
+    res.send(categorie)
+})
+
 app.listen(port, ()=>{
     console.log(`dragon api is runnng on port: ${port}`);
 })
